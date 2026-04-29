f332de4c466b84792f3465133adf22253e7eeb24
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-04-29 19:15:28 +0700

    setup font

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,6 @@ export default function Home() {
           style={{ backgroundImage: "url('/hero-bg.jpg')" }}
         />
         <div className="absolute inset-0 bg-[#0F131F]/50" />
-
         <header className="w-full h-20 bg-black/20 fixed top-0 left-0 flex items-center">
           <nav className="w-full flex flex-row justify-between items-center px-10">
             <Image src={"/logo-full.png"} alt="logo" width={85} height={85} />
@@ -30,7 +29,7 @@ export default function Home() {
               </li>
             </ul>
             <div className="bg-white py-2 px-3">
-              <span className="text-black">Login</span>
+              <span className="text-black font-cinzel">Login</span>
             </div>
           </nav>
         </header>
