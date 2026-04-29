4ea01d4cf0b546108e2b443aeb6ca00cae4c40a8
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-04-29 22:26:46 +0700

    areas section

--- src/app/page.js
+++ src/app/page.js
@@ -74,19 +74,21 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* about section */}
       <section className="w-full bg-white px-8 py-16">
-        <div className="grid grid-cols-12 gap-6">
+        <div className="grid-12">
           <div className="col-span-2">
-            <h6 className="uppercase font-semibold">Tentang Kami</h6>
+            <h6 className="section-title">Tentang Kami</h6>
           </div>
           <div className="col-start-7 col-span-6">
-            <h3 className="font-crimson-text text-4xl tracking-tight">
+            <h3 className="section-headline">
               Bango Parc adalah ruang di mana setiap perayaan terasa seperti di
               rumah sendiri
             </h3>
           </div>
         </div>
-        <div className="grid grid-cols-12 gap-6 mt-10">
+        <div className="grid-12 mt-10">
           <div className="col-span-6 w-full flex flex-row gap-5">
             <Image
               className="w-full"
@@ -117,15 +119,167 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* areas section */}
       <section className="w-full bg-white px-8 py-16">
-        <div>
-          <h6 className="uppercase font-semibold">Areas</h6>
+        <div className="mb-5">
+          <h6 className="section-title">Areas</h6>
         </div>
-        <div className="grid grid-cols-12 gap-6">
-          <h3></h3>
-          <p></p>
+        <div className="grid-12">
+          <div className="col-span-5 flex flex-col gap-3">
+            <h3 className="section-headline">
+              Temukan ruang yang tepat untuk setiap acara
+            </h3>
+            <p className="section-subheadline">
+              Dari suasana terbuka yang segar hingga ruang indoor yang intim —
+              Bango Parc menyediakan tiga pilihan area yang bisa didekorasi dan
+              disesuaikan sepenuhnya.
+            </p>
+          </div>
+        </div>
+        <div className="grid-12 mt-5">
+          <div className="relative col-span-4 w-full h-auto aspect-video bg-slate-200">
+            <div
+              style={{ backgroundImage: "url('/indoor.jpg')" }}
+              className="absolute inset-0 top-0 left-0 bg-cover bg-center"
+            />
+            <div className="bg-black/40 absolute inset-0 flex justify-center items-center">
+              <span className="font-crimson-text text-white text-2xl">
+                Indoor
+              </span>
+              <div className="absolute bottom-3 right-3 py-1.5 px-2.5 flex flex-row gap-2 bg-white items-center">
+                <svg
+                  className="w-5 h-5"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_148_32)">
+                    <path
+                      d="M32 42.6666H42.6667V53.3333"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M51.8879 29.4446C54.1812 23.1353 53.8132 17.2313 50.2879 13.7113C44.2292 7.64727 31.1279 10.9246 21.0266 21.0259C10.9253 31.1273 7.64792 44.2286 13.7093 50.2899C19.6479 56.2259 32.3412 53.2046 42.3492 43.5833"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_148_32">
+                      <rect width="64" height="64" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+
+                <span className="text-sm">EXPLORE 360°</span>
+              </div>
+            </div>
+          </div>
+          <div className="relative col-span-4 w-full h-auto aspect-video bg-slate-200">
+            <div
+              style={{ backgroundImage: "url(/semi-indoor.jpg)" }}
+              className="absolute inset-0 top-0 left-0 bg-cover bg-center"
+            />
+            <div className="bg-black/40 absolute inset-0 flex justify-center items-center">
+              <span className="font-crimson-text text-white text-2xl">
+                Semi-indoor
+              </span>
+              <div className="absolute bottom-3 right-3 py-1.5 px-2.5 flex flex-row gap-2 bg-white items-center">
+                <svg
+                  className="w-5 h-5"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_148_32)">
+                    <path
+                      d="M32 42.6666H42.6667V53.3333"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M51.8879 29.4446C54.1812 23.1353 53.8132 17.2313 50.2879 13.7113C44.2292 7.64727 31.1279 10.9246 21.0266 21.0259C10.9253 31.1273 7.64792 44.2286 13.7093 50.2899C19.6479 56.2259 32.3412 53.2046 42.3492 43.5833"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_148_32">
+                      <rect width="64" height="64" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+
+                <span className="text-sm">EXPLORE 360°</span>
+              </div>
+            </div>
+          </div>
+          <div className="relative col-span-4 w-full h-auto aspect-video bg-slate-200">
+            <div
+              style={{
+                backgroundImage: "url(/outdoor.jpg)",
+              }}
+              className="absolute inset-0 top-0 left-0 bg-center bg-cover"
+            />
+            <div className="bg-black/40 absolute inset-0 flex justify-center items-center">
+              <span className="font-crimson-text text-white text-2xl">
+                Outdoor
+              </span>
+              <div className="absolute bottom-3 right-3 py-1.5 px-2.5 flex flex-row gap-2 bg-white items-center">
+                <svg
+                  className="w-5 h-5"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_148_32)">
+                    <path
+                      d="M32 42.6666H42.6667V53.3333"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M51.8879 29.4446C54.1812 23.1353 53.8132 17.2313 50.2879 13.7113C44.2292 7.64727 31.1279 10.9246 21.0266 21.0259C10.9253 31.1273 7.64792 44.2286 13.7093 50.2899C19.6479 56.2259 32.3412 53.2046 42.3492 43.5833"
+                      stroke="black"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_148_32">
+                      <rect width="64" height="64" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+
+                <span className="text-sm">EXPLORE 360°</span>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+
+      {/* testimonials section */}
+      <section className="w-full bg-white px-8 py-16"></section>
     </main>
   );
 }
