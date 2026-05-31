06ad3fe8e48b2abcbc9d170956ce3d82d51aaf33
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-05-31 17:16:41 +0700

    chore(scratch): update reservation API debug script

--- scratch/test.js
+++ scratch/test.js
@@ -1,6 +1,11 @@
-fetch('https://bango-parc-service.vercel.app/api/payment')
-  .then(r => r.json())
-  .then(res => {
-    console.log("Payments API response:", JSON.stringify(res, null, 2));
-  })
-  .catch(err => console.error("Error:", err));
+async function run() {
+  const res = await fetch('https://bango-parc-service.vercel.app/api/reservation/all');
+  const data = await res.json();
+  console.log("Reservations count:", data.data?.length);
+  const sample = data.data?.[0] || {};
+  console.log("Keys in first reservation:", Object.keys(sample));
+  console.log("Sample reservation customer info:", JSON.stringify(sample.customer || sample.customerId, null, 2));
+  console.log("Full sample reservation:", JSON.stringify(sample, null, 2));
+}
+
+run();
