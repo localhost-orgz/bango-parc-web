44eb286f442f7728d1914514ccbf63c8dcd1b937
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-05-31 17:56:08 +0700

    chore(scratch): update reservation debug test script

--- scratch/test.js
+++ scratch/test.js
@@ -1,11 +1,13 @@
 async function run() {
   const res = await fetch('https://bango-parc-service.vercel.app/api/reservation/all');
   const data = await res.json();
-  console.log("Reservations count:", data.data?.length);
-  const sample = data.data?.[0] || {};
-  console.log("Keys in first reservation:", Object.keys(sample));
-  console.log("Sample reservation customer info:", JSON.stringify(sample.customer || sample.customerId, null, 2));
-  console.log("Full sample reservation:", JSON.stringify(sample, null, 2));
+  const list = data.data || [];
+  console.log("Reservations found:", list.length);
+  const withAreas = list.filter(r => r.areaReservations?.length > 0);
+  console.log("Reservations with areas:", withAreas.length);
+  if (withAreas.length > 0) {
+    console.log("Sample reservation with area:", JSON.stringify(withAreas[0], null, 2));
+  }
 }
 
 run();
